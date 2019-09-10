4aa5f294fbc8a70676a3391128b28afb95b70e26
Arczi <arczi92r@hotmail.com>
2019-09-10 22:29:14 +0200

    Updates

---
+++ precache-manifest.fd4ba5d63370addd042531075d807d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204b9c33ffa3f1dd1c37e1223e06e8b5",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "318005f041626bb28b34",
+    "url": "/react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f67d666da607e7d986d4",
+    "url": "/react/static/js/2.dd39abb8.chunk.js"
+  },
+  {
+    "revision": "318005f041626bb28b34",
+    "url": "/react/static/js/main.42089eaf.chunk.js"
+  },
+  {
+    "revision": "c8b84f4a3d1523c110ff",
+    "url": "/react/static/js/runtime~main.daedc5e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
